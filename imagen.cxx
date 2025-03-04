83ec97292e9473781d198eb973b5416f8bb581d1
advSNTS <187468015+advSNTS@users.noreply.github.com>
2025-03-04 15:27:30 -0500

    agregar this a imagen.cxx

--- imagen.cxx
+++ imagen.cxx
@@ -9,12 +9,12 @@ Imagen::Imagen(int ancho, int alto, int m_pixel, std::string nombreArchivo, std:
     this->alto = alto;
     this->m_pixel = m_pixel;
     this->nombreArchivo = nombreArchivo;
-    this -> imagen = imagen;
+    this-> imagen = imagen;
 }
 
 void Imagen::mostrar(){
     std::cout<<this->nombreArchivo<<"   Ancho: "<<this->ancho<<". Alto: "<<this->alto<<". M_pixel: "<<this->m_pixel<<std::endl<<". Imagen: "<<std::endl;
-    for(std::vector<std::vector<int>>::iterator it = imagen.begin(); it != imagen.end(); it++){
+    for(std::vector<std::vector<int>>::iterator it = this->imagen.begin(); it != this->imagen.end(); it++){
         for(std::vector<int>::iterator eso = it->begin(); eso != it->end(); eso++){
             std::cout<<*eso<<" ";
         }
